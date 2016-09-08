23ab71640c97200373b75ca8d8654092394f3393
JimiC <jimikar@gmail.com>
2016-09-08 17:17:19 +0300

    Add eslint and applies gulp rules to gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 /// <binding BeforeBuild='rebuild' Clean='clean' ProjectOpened='default' />
 'use strict';
 
-let paths = {
+var paths = {
     srcDir: './src/evetron',
     testsDir: './tests/evetron',
     packDir: './pack/',
     installBuilderDir: './dist/',
-    appDir: `${(process.platform === 'darwin'
+    appDir: (process.platform === 'darwin'
         ? 'Electron.app/Contents/Resources'
-        : 'resources')}/app`,
-    electron_dist: './node_modules/electron/dist',
+        : 'resources') + '/app',
+    electronDist: './node_modules/electron/dist',
     packagerDir: 'tools/packager',
-    platformResourcesDir: `resources/${process.platform}`,
+    platformResourcesDir: 'resources/' + process.platform,
     ts: {
         src: '/scripts/**/*.ts'
     },
@@ -21,18 +21,20 @@ let paths = {
     html: {
         src: '/html/**/*.html'
     },
-}
-paths.srcBinDir = `${paths.srcDir}/bin`;
-paths.testssrcBinDir = `${paths.testsDir}/bin`;
-paths.destDir = `${paths.srcBinDir}/${paths.appDir}`;
-paths.iconPath = `${paths.platformResourcesDir}/icon`;
+};
 
-let gulp = require('gulp'),
+paths.srcBinDir = paths.srcDir + '/bin';
+paths.testssrcBinDir = paths.testsDir + '/bin';
+paths.destDir = paths.srcBinDir + '/' + paths.appDir;
+paths.iconPath = paths.platformResourcesDir + '/icon';
+
+var gulp = require('gulp'),
     ncu = require('npm-check-updates'),
     fs = require('fs-extra'),
     del = require('del'),
     runsequence = require('run-sequence'),
-    tslint = require("gulp-tslint"),
+    eslint = require('gulp-eslint'),
+    tslint = require('gulp-tslint'),
     tsc = require('gulp-typescript'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
@@ -40,7 +42,7 @@ let gulp = require('gulp'),
     rename = require('gulp-rename'),
     packager = require('electron-packager'),
     builder = require('electron-builder'),
-    packageJson = require(`${paths.srcDir}/package.prod.json`);
+    packageJson = require(paths.srcDir + '/package.prod.json');
 
 
 /******* npm packages check ********/
@@ -72,12 +74,22 @@ gulp.task('install-typings', function () {
 });
 
 
+/******* eslint ********/
+
+gulp.task('eslint', function () {
+    return gulp.src(['gulpfile.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+
 /******* tslint ********/
 
-gulp.task("tslint", function () {
-    return gulp.src([`${paths.srcDir}${paths.ts.src}`])
+gulp.task('tslint', function () {
+    return gulp.src([paths.srcDir + paths.ts.src])
         .pipe(tslint())
-        .pipe(tslint.report("verbose", {
+        .pipe(tslint.report('verbose', {
             emitError: false,
             summarizeFailureOutput: true
         }));
@@ -87,23 +99,23 @@ gulp.task("tslint", function () {
 /******* src ********/
 
 gulp.task('clean:js', ['clean:js.map'], function () {
-    return del(`${paths.destDir}/**/*.js`);
+    return del(paths.destDir + '/**/*.js');
 });
 
 gulp.task('clean:js.map', function () {
-    return del(`${paths.destDir}/**/*.js.map`);
+    return del(paths.destDir + '/**/*.js.map');
 });
 
 gulp.task('clean:html', function () {
-    return del(`${paths.destDir}/**/*.html`);
+    return del(paths.destDir + '/**/*.html');
 });
 
 gulp.task('clean:css', ['clean:css.map'], function () {
-    return del(`${paths.destDir}/**/*.css`);
+    return del(paths.destDir + '/**/*.css');
 });
 
 gulp.task('clean:css.map', function () {
-    return del(`${paths.destDir}/**/*.css.map`);
+    return del(paths.destDir + '/**/*.css.map');
 });
 
 gulp.task('clean', function () {
@@ -111,7 +123,7 @@ gulp.task('clean', function () {
 });
 
 gulp.task('copy:html', ['clean:html'], function () {
-    return gulp.src(`${paths.srcDir}${paths.html.src}`)
+    return gulp.src(paths.srcDir + paths.html.src)
         .pipe(gulp.dest(paths.destDir));
 });
 
@@ -119,7 +131,7 @@ gulp.task('copy:html', ['clean:html'], function () {
 /******* sass ********/
 
 gulp.task('compile:sass', ['clean:css'], function () {
-    return gulp.src(`${paths.srcDir}${paths.scss.src}`)
+    return gulp.src(paths.srcDir + paths.scss.src)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('.', {
@@ -135,7 +147,7 @@ gulp.task('compile:sass', ['clean:css'], function () {
 /******* typescript ********/
 
 gulp.task('compile:ts', ['clean:js'], function () {
-    let tscProject = tsc.createProject(`${paths.srcDir}/tsconfig.json`);
+    var tscProject = tsc.createProject(paths.srcDir + '/tsconfig.json');
 
     return tscProject.src()
         .pipe(sourcemaps.init())
@@ -153,13 +165,13 @@ gulp.task('compile:ts', ['clean:js'], function () {
 /******* build ********/
 
 gulp.task('compile', ['compile:ts', 'compile:sass', 'copy:html'], function () {
-    return gulp.src(`${paths.srcDir}/package.prod.json`)
+    return gulp.src(paths.srcDir + '/package.prod.json')
         .pipe(rename('package.json'))
         .pipe(gulp.dest(paths.destDir));
 });
 
 gulp.task('build', ['compile'], function (cb) {
-    return fs.copy(paths.electron_dist, paths.srcBinDir,
+    return fs.copy(paths.electronDist, paths.srcBinDir,
         new RegExp('^(?!.+default_app\.asar)'),
         cb
     );
@@ -173,30 +185,30 @@ gulp.task('rebuild', function () {
 /******* watchers ********/
 
 gulp.task('watch:html', function () {
-    return gulp.watch([`${paths.srcDir}${paths.html.src}`], ['copy:html']);
+    return gulp.watch([paths.srcDir + paths.html.src], ['copy:html']);
 });
 
 gulp.task('watch:ts', function () {
-    return gulp.watch([`${paths.srcDir}${paths.ts.src}`], ['compile:ts', 'tslint']);
+    return gulp.watch([paths.srcDir + paths.ts.src], ['compile:ts', 'tslint']);
 });
 
 gulp.task('watch:sass', function () {
-    gulp.watch(`${paths.srcDir}${paths.scss.src}`, ['compile:sass']);
+    gulp.watch(paths.srcDir + paths.scss.src, ['compile:sass']);
 });
 
 gulp.task('watch:tests', function () {
-    return gulp.watch([`${paths.testsDir}${paths.ts.src}`], ['compile-tests', 'tslint']);
+    return gulp.watch([paths.testsDir + paths.ts.src], ['compile-tests', 'tslint']);
 });
 
 
 /******* tests ********/
 
 gulp.task('clean-tests', function () {
-    return del(`${paths.testssrcBinDir}`);
+    return del(paths.testssrcBinDir);
 });
 
 gulp.task('compile-tests', ['clean-tests'], function () {
-    let tscProject = tsc.createProject(`${paths.testsDir}/tsconfig.json`);
+    var tscProject = tsc.createProject(paths.testsDir + '/tsconfig.json');
 
     return tscProject.src()
         .pipe(sourcemaps.init())
@@ -215,28 +227,28 @@ gulp.task('pretest', function () {
 
 /******* packager ********/
 
-let packagerOptions = {
-    'dir': paths.destDir,
-    'platform': process.platform,
-    'arch': 'x64',
-    'name': packageJson.name,
+var packagerOptions = {
+    dir: paths.destDir,
+    platform: process.platform,
+    arch: 'x64',
+    name: packageJson.name,
     //version: packageJson.devDependencies['electron-prebuilt'],
-    //download: { cache: `./${paths.packagerDir}/cache` },
-    'ignore': '.+\.map',
-    'out': paths.packDir,
-    'icon': `./${paths.packagerDir}/${paths.iconPath}`,
-    'asar': true,
-    'overwrite': true,
-    'app-version': packageJson.version,
-    'app-copyright': packageJson.copyright,
-    'build-version': packageJson.version,
-    'version-string': {
-        'CompanyName': packageJson.author.name,
-        'FileDescription': packageJson.productName,
-        'ProductName': packageJson.productName,
-        'OriginalFilename': packageJson.name + '.exe'
+    //download: { cache: './' + paths.packagerDir + '/cache' },
+    ignore: '.+\.map',
+    out: paths.packDir,
+    icon: './' + paths.packagerDir + paths.iconPath,
+    asar: true,
+    overwrite: true,
+    appVersion: packageJson.version,
+    appCopyright: packageJson.copyright,
+    buildVersion: packageJson.version,
+    versionString: {
+        CompanyName: packageJson.author.name,
+        FileDescription: packageJson.productName,
+        ProductName: packageJson.productName,
+        OriginalFilename: packageJson.name + '.exe'
     },
-    'extend-info': `./${paths.packagerDir}/${paths.platformResourcesDir}/evetron.plist`
+    extendInfo: './' + paths.packagerDir + paths.platformResourcesDir + '/evetron.plist'
 };
 
 gulp.task('clean:package', function () {
@@ -250,25 +262,25 @@ gulp.task('package', ['clean:package', 'compile'], function (cb) {
 
 /******* installer *******/
 
-let installerOptions = {
-    'devMetadata': {
-        'build': {
-            'win': {
+var installerOptions = {
+    devMetadata: {
+        build: {
+            win: {
                 // README: https://github.com/electron-userland/electron-builder/wiki/Options#buildwin
-                'iconUrl': `${packageJson.repository}/blob/master/${paths.packagerDir}/${paths.iconPath}.ico?raw=true`
+                iconUrl: packageJson.repository + '/blob/master/' + paths.packagerDir + paths.iconPath + '.ico?raw=true'
             },
-            'linux': {
+            linux: {
                 // README: https://github.com/electron-userland/electron-builder/wiki/Multi-Platform-Build#linux
                 // https://github.com/electron-userland/electron-builder/wiki/Options#buildlinux
-                'target': ['deb', 'rpm']
+                target: ['deb', 'rpm']
             },
-            'mac': {
+            mac: {
                 // README: https://github.com/electron-userland/electron-builder/wiki/Multi-Platform-Build#macos
                 // https://github.com/electron-userland/electron-builder/wiki/Options#buildmac
             }
         },
         directories: {
-            buildResources: `./${paths.packagerDir}/${paths.platformResourcesDir}`,
+            buildResources: './' + paths.packagerDir + paths.platformResourcesDir,
             app: paths.destDir,
             output: paths.installBuilderDir
         }
@@ -281,11 +293,11 @@ gulp.task('clean:installer', function () {
 
 gulp.task('installer', ['clean:installer', 'compile'], function () {
     return builder.build(installerOptions)
-        .then(() => {
-            console.log(`Installer for ${process.platform} created successfully.`);
+        .then(function (){
+            console.log('Installer for ' + process.platform + ' created successfully.');
         })
-        .catch((error) => {
-            console.log(`Error: ${error.message}`);
+        .catch(function (error) {
+            console.log('Error: ' + error.message);
         });
 });
 
